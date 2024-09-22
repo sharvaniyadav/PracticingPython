edad015633c816c5409940ec4ed5d26e99675711
Sharvani Yadav <s1yadav@torontomu.ca>
2024-09-22 18:07:21 -0400

    Create PythonVariableNames.py

---
+++ PythonVariableNames.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 17:59:16 2024
+
+@author: Sharvani Yadav
+"""
+
+# Variable Names:
+    
+"""
+A variable can have a short name (like x and y) 
+or a more descriptive name (age, carname, total_volume). 
+
+Rules for Python variables:
+A variable name MUST start with a letter or the underscore character
+A variable name CANNOT start with a number
+A variable name can ONLY contain alpha-numeric characters and underscores
+Variable names are case-sensitive (age, Age and AGE are three different variables)
+A variable name cannot be any of the Python keywords
+
+"""
+# Legal Variables:
+    
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+# Illegal Variables:
+# 2myvar = "John"
+# my-var = "John"
+# my var = "John"
+
+# Multi Word Variable Names:
+    
+# Camel Case - Each word, except the first, starts with a capital letter:
+myVariableName = "John"
+
+# Pascal Case - Each word starts with a capital letter:
+MyVariableName = "John"
+
+# Snake Case - Each word is separated by an underscore character:
+my_variable_name = "John"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
