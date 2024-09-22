690749e06fd570b476f38ab2283a780c837f953d
Sharvani Yadav <s1yadav@torontomu.ca>
2024-09-22 17:38:02 -0400

    Create PythonSyntaxCommentsVariablesPractive.py
    This code is a practice on basics pertaining to python. Introduces importance of correct syntax formatting, indentation, and comments. It also introduced variables. Variables will be further elaborated later on!

---
+++ PythonSyntaxCommentsVariablesPractive.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 22 17:14:13 2024
+
+@author: Sharvani Yadav
+"""
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# Python Syntax:
+    
+print("helllo world")
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+
+# Python Indentation (Using if statements):
+    
+if 5 > 2:
+    print("Five is greater than two") # always indent after an if statement - otherwise error will show
+
+if 4 < 5:
+            print("4 is less than 5") # I can add more indentation and it will not show error
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+
+# Python Variables:
+    
+x = 5 # interger varaible of size 1 (see table window on the right)
+y = "Hello World!" # string variable of size 12 (see table on the right)
+
+# Note: Python has no comman for declaring variables
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+# Practicing Comments:
+"""
+This is a comment written
+in more than one line. By adding
+three quotations marks. I can type longer
+messages without being given a syntax error!
+"""
+
